051d2d76a27a1cd119ec0d6170ca32486e6f663c
csslick <digitone@naver.com>
2023-08-11 21:52:09 +0900

    main 영역
    숙제: col을 자식 컴퍼넌트로 만들어 보세요 -> props로 data 전달

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,28 @@ import New from './components/New';
 import Main from './components/Main';
 function App() {
 
+  // new 글목록 데이터 - New 컴포넌트에 전달할 데이터
+  const news = [
+    { 
+      title: 'Hydrogen VS Electric Cars',
+      text: 'Will hydrogen-fueled cars ever catch up to EVs?',
+    },
+    { 
+      title: 'The Downsides of AI Artistry',
+      text: 'What are the possible adverse effects of on-demand AI image generation?',
+    },
+    { 
+      title: 'Is VC Funding Drying Up?',
+      text: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.',
+    },
+  ]
+
   return (
     <>
       <Header/>
       <div className='container intro-box'>
         <Intro/>
-        <New/>
+        <New news={news} />
       </div>
       <Main/>
     </>
--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,8 +1,24 @@
 
-export default function New() {
+export default function New(props) {
+  console.log(props);
+
   return (
     <aside className='new'>
       <h1>New</h1>
+      <ul>
+        <li>
+          <h4>{props.news[0].title}</h4>
+          <p>{props.news[0].text}</p>
+        </li>
+        <li>
+          <h4>{props.news[1].title}</h4>
+          <p>{props.news[1].text}</p>
+        </li>
+        <li>
+          <h4>{props.news[2].title}</h4>
+          <p>{props.news[2].text}</p>
+        </li>
+      </ul>
     </aside>
   )
 }
